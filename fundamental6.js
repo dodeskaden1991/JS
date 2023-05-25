13821f03e045734b965edd8570055a483b8e0990
dodeskaden1991 <dodeskaden1991@gmail.com>
2023-05-26 00:55:49 +0300

    добавен сайт и задача от softuni
    More Exercises: Data Types and Variables - решена първа задача и добавен сайт за играта 8-те царици.

---
+++ fundamental6.js
@@ -0,0 +1,18 @@
+//More Exercises: Data Types and Variables
+function digits(word){
+    const result = {
+        'one': 1,
+        'two': 2,
+        'three': 3,
+        'four': 4,
+        'five': 5,
+        'six': 6,
+        'seven': 7,
+        'eight': 8,
+        'nine': 9
+    }
+    result.hasOwnProperty(word) ? console.log(result[word]) : undefined
+}
+
+
+digits("one")
