ca2cae3f028989b1337963637f6ebd854052cc3c
dodeskaden1991 <dodeskaden1991@gmail.com>
2023-05-27 00:12:38 +0300

    Update fundamental6.js
    Types and Variables - More Exercise: решени до задача 5

--- fundamental6.js
+++ fundamental6.js
@@ -14,5 +14,58 @@ function digits(word){
     result.hasOwnProperty(word) ? console.log(result[word]) : undefined
 }
 
+function primary(number){
+    primaryChecker = number - 1
+    while(primaryChecker>=0){
+        if(primaryChecker <= 1){
+            console.log("true")
+            return;
+        }else if(number % primaryChecker == 0){
+            console.log("false")
+            return;
+        }
+        primaryChecker --
+    }
+}
 
-digits("one")
+function cone(radius, height){
+    const parametry = {
+        V: {
+            result: (r, h) => {
+                return (1/3)*Math.PI*Math.pow(r,2)*h
+            }
+        },
+        S: {
+            result: (r, h) => {
+                return Math.sqrt(Math.pow(r,2)+Math.pow(h,2))
+            }
+        },
+        L: {
+            result: (r) => {
+                return Math.PI * r * parametry.S.result(radius,height)
+            }
+        },
+        B: {
+            result: (r) => {
+                return Math.PI * Math.pow(r,2)
+            }
+        },
+        A: {
+            result: (r, h) => {
+                return parametry.L.result(radius) + parametry.B.result(radius)
+            }
+        },
+    }
+    console.log(`volume = ${parametry["V"].result(radius,height).toFixed(4)}`)
+    console.log(`area = ${parametry["A"].result(radius,height).toFixed(4)}`)
+}
+
+function biggest(){
+    console.log(Math.max(...arguments))
+} 
+
+function binary(number){
+    console.log(parseInt(number,2))
+} 
+
+binary(101101001)
