6c3fe016ba8112798347eb7a27db43721331cdce
dodeskaden1991 <dodeskaden1991@gmail.com>
2023-10-20 19:59:04 +0300

    ???

--- functions.js
+++ functions.js
@@ -94,9 +94,9 @@ sorting()
 let blah = 0
 
 const myArray = [['2', '3', '4', '7', '0'],
-    ['4', '0', '5', '3', '1'],
-    ['2', '3', '1', '4', '0'],
-    ['9', '8', '5', '2', '4']]
+['4', '0', '5', '3', '1'],
+['2', '3', '1', '4', '0'],
+['9', '8', '5', '2', '4']]
 
 const containsDuplicate = (array) => {
     for (const subArray of array) {
@@ -115,3 +115,92 @@ const containsDuplicate = (array) => {
 }
 
 console.log(containsDuplicate(myArray)) // true
+
+const person = {
+    fullName: function (city, country) {
+        return this.firstName + " " + this.lastName + " : " + city + ", " + country;
+    }
+}
+
+const person1 = {
+    firstName: "John",
+    lastName: "Doe"
+}
+
+// Това ще върне "John Doe,Oslo,Norway"
+console.log(person.fullName.call(person1, "Oslo", "Norway"))
+console.log(person.fullName.apply(person1, ["Burgas", "Bulgaria"]))
+
+const obj = {
+    name: 'Peter',
+    outer() {
+        console.log(this); // Object {name: "Peter"}
+        const inner = () => console.log(this);
+        inner();
+    }
+}
+obj.outer(); // Object {name: "Peter"}
+
+const obj2 = {
+    name: 'Peter',
+    outer() {
+        console.log(this); // Object {name: "Peter"}
+        const inner = function () { console.log(this); }
+        inner();
+    }
+}
+obj2.outer(); // Window
+
+function sayHello(...name) {
+    console.log("Здравей, " + name + ". Това е " + this);
+}
+
+var test_this = "Това е this, то може и да е обикновена променлива";
+var names = {
+    1: 'Deyan',
+    2: "Dragan",
+    3: "Petkan"
+}
+
+// Това ще изведе "Здравей, Deyan. Това е Bing" и т.н. за всеки елемент от names
+sayHello.apply(test_this, Object.values(names))
+
+//пример как се презаписват име и професия
+const firstPerson = {
+    name: "Peter",
+    prof: "Fisherman",
+    shareInfo: function () {
+    console.log(`${this.name} works as a ${this.prof}`);
+    }
+    };
+    const secondPerson = { name: "George", prof: "Manager" };
+    firstPerson.shareInfo.apply(secondPerson); // подава данните за this
+
+
+
+    const person_bind = {
+        fullName: function(city, country) {
+        return this.firstName + " " + this.lastName + "," + city + "," + country;
+        }
+        }
+        
+        const member = {
+        firstName:"Hege",
+        lastName: "Nilsen",
+        }
+
+        const member2 ={
+            firstName: "Hege2",
+            lastName: "Nilsen2",
+        }
+        
+        let fullName_static = person_bind.fullName.bind(member, "Oslo", "Norway");
+        let fullName_dynamic = person_bind.fullName.bind(member);
+        let fullName2 = person_bind.fullName.apply(member, ["Oslo", "Norway"]);
+        let fullName3 = person_bind.fullName.call(member, "Oslo", "Norway");
+        console.log(fullName_static()); // Hege Nilsen,Oslo,Norway
+        console.log(fullName_static(1,2)); // Hege Nilsen,Oslo,Norway
+        console.log(fullName2); // Hege Nilsen,Oslo,Norway
+        console.log(fullName3); // Hege Nilsen,Oslo,Norway
+        console.log(fullName_dynamic()); // Hege Nilsen,undefined,undefined
+        console.log(fullName_dynamic(1,2)); // Huge Nilsen,1,2
